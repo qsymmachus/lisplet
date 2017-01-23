6fa102a25140f73548ec700d96dfe3f94d6613f8
John Olmsted <john@olmsted.io>
2017-01-22 17:25:09 -0800

    Adds basic polish notation parsers

--- lisplet.c
+++ lisplet.c
@@ -1,29 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <editline/readline.h>
 #include "libraries/mpc.h"
 
 /* Lisplet semantic version */
-const char* version = "0.0.1";
-
-/* Read, Evaluate, Print, Loop */
-void repl() {
-  printf("Lisplet v%s\n", version);
-  puts("Press Ctrl+C to exit\n");
-
-  while(1) {
-    char* input = readline("lisplet> ");
-    add_history(input);
-
-    printf("You input %s\n", input);
-
-    free(input);
-  }
-}
+const char* version = "0.0.2";
 
 /* Main function */
 int main(int argc, char** argv) {
-  repl();
+
+  /* Create our parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lisplet  = mpc_new("lisplet");
+  
+  /* Define our parsers with the following grammar */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lisplet  : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lisplet);
+  
+  /* Read, Evaluate, Print Loop */
+  printf("Lisplet v%s\n", version);
+  puts("Press Ctrl+c to Exit\n");
+  
+  while (1) {
+    char* input = readline("lispy> ");
+    add_history(input);
+    
+    /* Parse user input */
+    mpc_result_t r;
+
+    if (mpc_parse("<stdin>", input, Lisplet, &r)) {
+      /* On success, print the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print the error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
+    free(input);
+  }
+  
+  /* Undefine and delete our parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lisplet);
+  
   return 0;
 }
-
